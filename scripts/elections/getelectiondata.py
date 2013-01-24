ea0785df989893a32d1e1795cb3a322f4146785e
Andrei Cipu <acipu@ixiacom.com>
2013-01-24 15:20:02 +0200

    Finalize parsing the CSV

--- scripts/elections/getelectiondata.py
+++ scripts/elections/getelectiondata.py
@@ -32,7 +32,7 @@ county_names = [
     u"Neamț",
     u"Olt",
     u"Prahova",
-    u"Satu Mare",
+    u"Satu-Mare",
     u"Sălaj",
     u"Sibiu",
     u"Suceava",
@@ -45,6 +45,41 @@ county_names = [
 ]
 
 import os
+import csv
+
+def fetch_data(file_prefix, county, year, county_no, village):
+    out_file = file_prefix + u".html"
+    wget_comm = u"wget -O " + out_file + u" \"http://www.roaep.ro/alegeri/judete/" + year + u"loc/" + year + u"locp_xls.php?judet=" + county + u"&jud=" + str(county_no) + u"&aleg=1&den_aleg=Primar&circ=" + str(village) + u"\""
+    print wget_comm
+    os.system(wget_comm.encode("utf8"))
+    convert_comm = u"python html2csv.py " + out_file + "; rm -rf " + out_file 
+    print convert_comm
+    os.system(convert_comm.encode("utf8"))
+
+def parse_csv(file_prefix):
+    in_file = file_prefix + u".csv"
+    print in_file
+    cr = csv.reader(open(in_file,"rb"))
+    elem = [name for line in cr for name in line]
+    #print elem
+    try:
+        county_no = int(elem[5])
+        print u"Judet " + elem[7] + "; localitate " + elem[11]
+    except:
+        print "Fake file!"
+        return
+    if u'Tur 2' in elem:
+        elem2 = elem[elem.index(u'Tur 2') + 1:]
+        #print elem2
+    else:
+	elem2 = elem
+    	#print elem2
+    if elem2.index(u'%') + 2 > len(elem2):
+        elem2 = elem #go back to round 1
+    winner = elem2[elem2.index(u'%') + 1]
+    party = elem2[elem2.index(u'%') + 2]
+
+    print winner + " (" + party + ")"
 
 if __name__ == "__main__":
     county_no = 0
@@ -52,10 +87,6 @@ if __name__ == "__main__":
         county_no += 1
         for year in [u"96", u"00", u"04", u"08"]:
             for village in range(103):
-                out_file = year + county + str(village) + u".html"
-                wget_comm = u"wget -O " + out_file + u" \"http://www.roaep.ro/alegeri/judete/" + year + u"loc/" + year + u"locp_xls.php?judet=" + county + u"&jud=" + str(county_no) + u"&aleg=1&den_aleg=Primar&circ=" + str(village) + u"\""
-                print wget_comm
-                os.system(wget_comm.encode("utf8"))
-                convert_comm = u"python html2csv.py " + out_file
-                print convert_comm
-                os.system(convert_comm.encode("utf8"))
+                file_prefix = year + county + str(village + 1)
+                fetch_data(file_prefix, county, year, county_no, village + 1)
+                parse_csv(file_prefix)
