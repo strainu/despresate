7dcba55cbfed586f323ef856bd7fc27c0f32aa93
Andrei Cipu <acipu@ixiacom.com>
2013-01-30 11:03:35 +0200

    Find the SIRUTA code for the mayor data

--- scripts/elections/getelectiondata.py
+++ scripts/elections/getelectiondata.py
@@ -46,6 +46,7 @@ county_names = [
 
 import os
 import csv
+import sirutalib
 
 def fetch_data(file_prefix, county, year, county_no, village):
     out_file = file_prefix + u".html"
@@ -58,15 +59,14 @@ def fetch_data(file_prefix, county, year, county_no, village):
 
 def parse_csv(file_prefix, out_file, year):
     in_file = file_prefix + u".csv"
-    print in_file
+    #print in_file.encode("utf8")
     cr = csv.reader(open(in_file,"rb"))
-    elem = [name for line in cr for name in line]
+    elem = [name.decode("utf8") for line in cr for name in line]
     #print elem
     try:
         county_no = int(elem[5])
-        print u"Judet " + elem[7] + "; localitate " + elem[11]
     except:
-        print "Fake file!"
+        #print "Fake file!"
         return
     if u'Tur 2' in elem:
         elem2 = elem[elem.index(u'Tur 2') + 1:]
@@ -80,20 +80,46 @@ def parse_csv(file_prefix, out_file, year):
     party = elem2[elem2.index(u'%') + 2]
 
     county = elem[7].strip().upper()
-    if county.find("JUDETUL") == -1:
-	county = "JUDETUL " + county
+    if county.find(u"JUDETUL") == -1:
+	if county.find(u"JUDET") == -1:
+            county = u"JUDETUL " + county
+        else:
+            county = country.replace("JUDET", "JUDETUL")
 
+    #print county + u"; localitate " + elem[11]
     out_file.write(u"\"" + county + u"\",\"" + elem[11] + u"\",\"" + winner + u"\",\"" + party + u"\",\"")
     if year == u"96":
 	out_file.write("1996\"\n")
     else:
         out_file.write("20" + year + "\"\n")
 
-    print winner + " (" + party + ")"
+    #print winner + " (" + party + ")"
+
+def find_siruta():
+    dia_trans = {ord(u"Ș"): u"S", ord(u"ș"): u"s", ord(u"Ț"): u"T", ord(u"ț"): u"t", 
+		ord(u"Ş"): u"S", ord(u"ş"): u"s", ord(u"Ţ"): u"T", ord(u"ţ"): u"t", 
+		ord(u"Ă"): u"A", ord(u"Â"): u"A", ord(u"Î"): u"I", 
+		ord(u"ă"): u"a", ord(u"â"): u"a", ord(u"î"): u"i"}
+    data = {}
+    _csv = sirutalib.SirutaDatabase()
+    #TODO: this is based on the assumption that we don't have 2 communes with the same name in the same county
+    for elem in _csv._data:
+	index = _csv._data[elem]["name"].translate(dia_trans).replace("MUNICIPIUL", "XXX").replace("ORAS", "XXX") + \
+		_csv.get_county_string(elem).translate(dia_trans).replace("MUNICIPIUL", "XXX").replace("ORAS", "XXX")
+	data[index] = elem
+	print index
+    cr = csv.reader(open("primari_tmp.csv", "rb"))
+    cw = csv.writer(open("primari.csv", "wb"))
+    for line in cr:
+	index = line[1].replace("MUNICIPIUL", "XXX").replace("ORAS", "XXX") + line[0]
+	if index in data:
+            line.append(data[index])
+	cw.writerow(line)
+    pass
 
 if __name__ == "__main__":
     county_no = 0
-    of = open("primari.csv", "w+")
+    of = open("primari_tmp.csv", "w+")
     for county in county_names:
         county_no += 1
         for year in [u"96", u"00", u"04", u"08"]:
@@ -102,3 +128,4 @@ if __name__ == "__main__":
                 #fetch_data(file_prefix, county, year, county_no, village + 1)
                 parse_csv(file_prefix, of, year)
     of.close()
+    find_siruta()
