efe9ace198901fd2e1573dc082d5d4ad092bd4bd
Andrei Cipu <github@strainu.ro>
2013-06-24 21:12:46 +0300

    Transform all database queries into functions.

--- judet.php
+++ judet.php
@@ -6,132 +6,257 @@ require('./include/common.php');
 require('smarty/libs/Smarty.class.php');
 
 if (!isset($_GET['id']) || !is_numeric($_GET['id']) || $_GET['id'] <= 0 || $_GET['id'] > 52)
-	$index = 40;
+    $index = 40;
 else
-	$index = $_GET['id'];
+    $index = $_GET['id'];
     
 $smarty = new Smarty();
-	
+
 $MyObject = new SimpleSQL( $dbs, $dbu, $dbp, $db, 0, 0 );
-$MyObject->Query("SELECT * FROM `judet` FULL JOIN `siruta` ON `siruta`.`_siruta`=`siruta` WHERE `index`=".$index." LIMIT 1");
-$county_data = $MyObject->getCurrentLine();
-if ($county_data == -1) {
-	echo "Problem fetching county data";
-	exit(1);
-}
 
-$MyObject->Query("SELECT `_siruta`, `denloc` FROM `siruta` WHERE `sirsup`=".$county_data['siruta']);
-$uat_data = $MyObject->getTable();
-if ($uat_data == -1) {
-	echo "Problem fetching UAT data";
-	exit(1);
-}
+function county_data($index) {
+    global $MyObject;
 
-$MyObject->Query("SELECT * FROM `demografie` WHERE `siruta`=".$county_data['siruta']." ORDER BY `an` DESC");
-$pop = $MyObject->getTable();
-if ($pop == -1) {
-	echo "Problem fetching population data";
-	exit(1);
-}
-
-$MyObject->Query("SELECT `nume` FROM `regiuni` WHERE `regiune`=".$county_data['regiune']." OR `regiune`=".$county_data['regiune_istorica']." ORDER BY `regiune`");
-$region = $MyObject->getElement(0, 'nume');
-$hist_region = $MyObject->getElement(1, 'nume');
-if ($region == -1 or $hist_region == -1) {
-	echo "Problem fetching region data";
-	exit(1);
-}
-
-$MyObject->Query("SELECT * FROM `imagini` WHERE `county`=".$county_data['jud']." ORDER BY RAND() DESC LIMIT 12");
-$images = $MyObject->getTable();
-if ($images == -1) {
-	echo "Problem fetching image data";
-	exit(1);
-}
-
-$MyObject->Query("SELECT * FROM `oameni` WHERE `siruta`=".$county_data['siruta']." ORDER BY `an` DESC");
-$leaders = $MyObject->getTable();
-if ($leaders == -1) {
-	echo "Problem fetching county president";
-	exit(1);
-}
-$cjpresyear = 0;
-$cjviceyear = 0;
-$cjpresid = 0;
-$cjvice = Array();
-$cjmembers = Array();
-$cjmyear = 0;
-$prpresyear = 0;
-$prpresid = 0;
-$prviceyear = 0;
-$prviceid = 0;
-foreach ($leaders as $leader) {
-	switch($leader["functie"]) {
-		case 4:
-			if ($cjpresyear)
-				continue;//TODO: history
-			$cjpres = $leader['nume'];
-			$cjpresparty = $leader['partid'];
-			$cjpresyear = $leader['an'];
-			$cjpresid = $leader['agenda_id'];
-		break;
-		case 5:
-			if ($cjviceyear != 0 && $cjviceyear != $leader['an'])
-				continue;//TODO: history
-			array_push($cjvice, Array( "name" => $leader['nume'],  "party" => $leader['partid'], "id" => $leader['agenda_id']));
-			$cjviceyear = $leader['an'];
-		break;
-		case 6:
-			if ($cjmyear != 0 && $cjmyear != $leader['an'])
-				continue;//TODO: history
-			array_push($cjmembers, Array( "name" => $leader['nume'],  "party" => $leader['partid'], "id" => $leader['agenda_id']));
-			$cjmyear = $leader['an'];
-		break;
-		case 7:
-			if ($prpresyear)
-				continue;
-			$prpres = $leader['nume'];
-			$prpresyear = $leader['an'];
-			$prpresid = $leader['agenda_id'];
-		break;
-		case 8:
-			if ($prviceyear)
-				continue;
-			$prvice = $leader['nume'];
-			$prviceyear = $leader['an'];
-			$prviceid = $leader['agenda_id'];
-		break;
-	}
-}
-
-$MyObject->Query("SELECT * FROM `monumente` LEFT JOIN `imagini` ON monumente.imagine = imagini.index WHERE monumente.cod LIKE '".$county_data['prescurtare']."%' ORDER BY RAND() LIMIT 10");
-$monuments = $MyObject->getTable();
-if ($monuments == -1) {
-        echo "Problem fetching monument data";
+    $MyObject->Query("SELECT * FROM `judet` FULL JOIN `siruta` ON `siruta`.`_siruta`=`siruta` WHERE `index`=".$index." LIMIT 1");
+    $county_data = $MyObject->getCurrentLine();
+    if ($county_data == -1) {
+        echo "Problem fetching county data";
         exit(1);
+    }
+    
+    return $county_data;
 }
 
-$MyObject->Query("SELECT `denloc`,`jud` FROM `siruta` WHERE sirsup=1 ORDER BY `denloc`");
-$county_list = $MyObject->getTable();
-if ($county_list == -1) {
-	echo "Problem fetching county list";
-	exit(1);
+function county_uat_data($county_data) {
+    global $MyObject;
+
+    $MyObject->Query("SELECT `_siruta`, `denloc` FROM `siruta` WHERE `sirsup`=".$county_data['siruta']);
+    $uat_data = $MyObject->getTable();
+    if ($uat_data == -1) {
+        echo "Problem fetching UAT data";
+        exit(1);
+    }
+    
+    return $uat_data;
 }
-for($i = 0; $i < $MyObject->getNrLines(); $i++)
-	$county_list[$i]['denloc'] = capitalize_counties($county_list[$i]['denloc']);
+
+function county_population($county_data) {
+    global $MyObject;
+
+    $MyObject->Query("SELECT * FROM `demografie` WHERE `siruta`=".$county_data['siruta']." ORDER BY `an` DESC");
+    $pop = $MyObject->getTable();
+    if ($pop == -1) {
+        echo "Problem fetching population data";
+        exit(1);
+    }
+    
+    return $pop;
+}
+
+function county_region($county_data, &$region, &$hist_region) {
+    global $MyObject;
+
+    $MyObject->Query("SELECT `nume` FROM `regiuni` WHERE `regiune`=".$county_data['regiune']." OR `regiune`=".$county_data['regiune_istorica']." ORDER BY `regiune`");
+    $region = $MyObject->getElement(0, 'nume');
+    $hist_region = $MyObject->getElement(1, 'nume');
+    if ($region == -1 or $hist_region == -1) {
+        echo "Problem fetching region data";
+        exit(1);
+    }
+}
+
+function county_images($county_data)
+{
+    global $MyObject;
+
+    $MyObject->Query("SELECT * FROM `imagini` WHERE `county`=".$county_data['jud']." ORDER BY RAND() DESC LIMIT 12");
+    $images = $MyObject->getTable();
+    if ($images == -1) {
+        echo "Problem fetching image data";
+        exit(1);
+    }
+    
+    return $images;
+}
+
+function county_leaders($county_data)
+{
+    global $MyObject;
+
+    $MyObject->Query("SELECT * FROM `oameni` WHERE `siruta`=".$county_data['siruta']." ORDER BY `an` DESC");
+    $leaders = $MyObject->getTable();
+    if ($leaders == -1) {
+        echo "Problem fetching county president";
+        exit(1);
+    }
+    
+    return $leaders;
+}
+
+
+function process_county_leaders($leaders, 
+                                &$cjpres, &$cjpresyear, &$cjpresid, &$cjpresparty,
+                                &$cjvice, &$cjviceyear, &$cjmembers, &$cjmyear, 
+                                &$prpres, &$prpresyear, &$prpresid, 
+                                &$prvice, &$prviceyear, &$prviceid)
+{
+    $cjpres = "";
+    $cjpresyear = 0;
+    $cjpresid = 0;
+    $cjvice = Array();
+    $cjviceyear = 0;
+    $cjmembers = Array();
+    $cjmyear = 0;
+    $prpres = "";
+    $prpresyear = 0;
+    $prpresid = 0;
+    $prvice = "";
+    $prviceyear = 0;
+    $prviceid = 0;
+    
+    foreach ($leaders as $leader) {
+        switch($leader["functie"]) {
+            case 4:
+                if ($cjpresyear)
+                    continue;//TODO: history
+                $cjpres = $leader['nume'];
+                $cjpresparty = $leader['partid'];
+                $cjpresyear = $leader['an'];
+                $cjpresid = $leader['agenda_id'];
+            break;
+            case 5:
+                if ($cjviceyear != 0 && $cjviceyear != $leader['an'])
+                    continue;//TODO: history
+                array_push($cjvice, Array( "name" => $leader['nume'],  "party" => $leader['partid'], "id" => $leader['agenda_id']));
+                $cjviceyear = $leader['an'];
+            break;
+            case 6:
+                if ($cjmyear != 0 && $cjmyear != $leader['an'])
+                    continue;//TODO: history
+                array_push($cjmembers, Array( "name" => $leader['nume'],  "party" => $leader['partid'], "id" => $leader['agenda_id']));
+                $cjmyear = $leader['an'];
+            break;
+            case 7:
+                if ($prpresyear)
+                    continue;
+                $prpres = $leader['nume'];
+                $prpresyear = $leader['an'];
+                $prpresid = $leader['agenda_id'];
+            break;
+            case 8:
+                if ($prviceyear)
+                    continue;
+                $prvice = $leader['nume'];
+                $prviceyear = $leader['an'];
+                $prviceid = $leader['agenda_id'];
+            break;
+        }
+    }
+}
+
+/**
+ * county_monuments - retrieve a list of maximum 10 historic monuments from that county
+ *
+ * @param Array $county_data 
+ * @return all monuments from the database
+ */
+function county_monuments($county_data)
+{
+    global $MyObject;
+
+    $MyObject->Query("SELECT * FROM `monumente` LEFT JOIN `imagini` ON monumente.imagine = imagini.index WHERE monumente.cod LIKE '".$county_data['prescurtare']."%' ORDER BY RAND() LIMIT 10");
+    $monuments = $MyObject->getTable();
+    if ($monuments == -1) {
+            echo "Problem fetching monument data";
+            exit(1);
+    }
+    
+    return $monuments;
+}
+
+function county_list()
+{
+    global $MyObject;
+
+    $MyObject->Query("SELECT `denloc`,`jud` FROM `siruta` WHERE sirsup=1 ORDER BY `denloc`");
+    $county_list = $MyObject->getTable();
+    if ($county_list == -1) {
+        echo "Problem fetching county list";
+        exit(1);
+    }
+    for($i = 0; $i < $MyObject->getNrLines(); $i++)
+        $county_list[$i]['denloc'] = capitalize_counties($county_list[$i]['denloc']);
+
+    return $county_list;
+}
+
+function county_density($county_data, $pop)
+{
+    if($county_data['suprafata'])
+        $density = $pop[0]['populatie'] / $county_data['suprafata'];
+    else
+        $density = '';
+        
+    return $density;
+}
+
+//call the function that retrieve data from the database
+$county_data = county_data($index);
+$uat_data = county_uat_data($county_data);
+$pop = county_population($county_data);
+county_region($county_data, $region, $hist_region);
+$images = county_images($county_data);
+$leaders = county_leaders($county_data);
+$county_list = county_list();
+$monuments = county_monuments($county_data);
 
 $county_str = capitalize_counties($county_data['denloc']);
-
-
-if($county_data['suprafata'])
-    $density = $pop[0]['populatie'] / $county_data['suprafata'];
-else
-    $density = '';
 $short_name = str_ireplace("Județul ", "", $county_str);
-
+$density = county_density($county_data, $pop);
 
 $type = filter_input(INPUT_GET, 't', FILTER_SANITIZE_STRING);
 $format = filter_input(INPUT_GET, 'f', FILTER_SANITIZE_STRING);
+
+function county_generate_stats_csv_header()
+{
+    return "nume,prescurtare,suprafata,populatie,populatie_an,densitate,regiune_adm,regiune_ist,wikipedia\n";
+}
+
+function county_generate_stats_csv($county_data, $pop, $region, $hist_region)
+{
+    $county_str = capitalize_counties($county_data['denloc']);
+    $density = county_density($county_data, $pop);
+    $data = $county_str.','.$county_data['prescurtare'].',"';
+    $data += $county_data['suprafata'].'",';
+    $data += $pop[0]['populatie'].','.$pop[0]['an'].',"'.$density.'",';
+    $data += $region.','.$hist_region.',"ro:'.$county_str."\"\n";
+    
+    return $data;
+}
+
+function county_generate_leaders_csv_header()
+{
+    return "poziție,nume,an\n";
+}
+
+function county_generate_leaders_csv($county_data, $leaders)
+{
+    process_county_leaders($leaders, 
+                            &$cjpres, &$cjpresyear, &$cjpresid, &$cjpresparty,
+                            &$cjvice, &$cjviceyear, &$cjmembers, &$cjmyear, 
+                            &$prpres, &$prpresyear, &$prpresid, 
+                            &$prvice, &$prviceyear, &$prviceid);
+
+    echo $county_data['siruta'].',prefect,'.$prpres.','.$prpresyear."\n";
+    echo $county_data['siruta'].',subprefect,'.$prvice.','.$prviceyear."\n";
+    echo $county_data['siruta'].',președinte consiliu județean,'.$cjpres.','.$cjpresyear."\n";
+    foreach ($cjvice as $leader) {
+        echo $county_data['siruta'].',vicepreședinte consiliu județean,'.$leader['name'].','.$cjviceyear."\n";
+    }
+    foreach ($cjmembers as $leader) {
+        echo $county_data['siruta'].',membru consiliu județean,'.$leader['name'].','.$cjmyear."\n";
+    }
+}
 
 switch($format)
 {
@@ -141,30 +266,19 @@ switch($format)
         switch($type)
         {
             case 'stats':
-                echo "siruta,nume,prescurtare,suprafata,populatie,populatie_an,densitate,regiune_adm,regiune_ist,wikipedia\n";
-                echo $county_data['siruta'].','.$county_str.','.$county_data['prescurtare'].',"';
-                echo $county_data['suprafata'].'",';
-                echo $pop[0]['populatie'].','.$pop[0]['an'].',"'.$density.'",';
-                echo $region.','.$hist_region.',"ro:'.$county_str."\"\n";
+                echo "siruta,".county_generate_stats_csv_header();
+                echo $county_data['siruta'].",".county_generate_stats_csv($county_data, $pop, $region, $hist_region);
             break;
             case 'leaders':
-                echo "siruta,poziție,nume,an\n";
-                echo $county_data['siruta'].',prefect,'.$prpres.','.$prpresyear."\n";
-                echo $county_data['siruta'].',subprefect,'.$prvice.','.$prviceyear."\n";
-                echo $county_data['siruta'].',președinte consiliu județean,'.$cjpres.','.$cjpresyear."\n";
-                foreach ($cjvice as $leader) {
-                    echo $county_data['siruta'].',vicepreședinte consiliu județean,'.$leader['name'].','.$cjviceyear."\n";
-                }
-                foreach ($cjmembers as $leader) {
-                    echo $county_data['siruta'].',membru consiliu județean,'.$leader['name'].','.$cjmyear."\n";
-                }
+                echo "siruta,".county_generate_leaders_csv_header();
+                echo $county_data['siruta'].",".county_generate_leaders_csv($county_data, $leaders);
             break;
             case 'all':
             default:
-                echo "siruta,prescurtare,suprafata,populatie,populatie_an,densitate,regiune_adm,regiune_ist,wikipedia\n";
-                echo $county_data['siruta'].','.$county_data['prescurtare'].',"'.$county_data['suprafata'].'",';
-                echo $pop[0]['populatie'].','.$pop[0]['an'].',"'.$density.'",';
-                echo $region.','.$hist_region.',"ro:'.$county_str."\"\n";
+                echo "siruta,".county_generate_stats_csv_header().",".county_generate_leaders_csv_header();
+                echo $county_data['siruta'].",".
+                        county_generate_stats_csv($county_data, $pop, $region, $hist_region).",".
+                        county_generate_leaders_csv($county_data, $leaders);
             break;
         };
     break;
@@ -174,6 +288,12 @@ switch($format)
     break;
     case 'html':
     default:
+
+        process_county_leaders($leaders, 
+                            &$cjpres, &$cjpresyear, &$cjpresid, &$cjpresparty,
+                            &$cjvice, &$cjviceyear, &$cjmembers, &$cjmyear, 
+                            &$prpres, &$prpresyear, &$prpresid, 
+                            &$prvice, &$prviceyear, &$prviceid);
         $smarty->assign('name', $county_str);
         $smarty->assign('index', $index);
         $smarty->assign('region', $region);
--- sat.php
+++ sat.php
@@ -11,92 +11,166 @@ if (!isset($_GET['siruta']) || !is_numeric($_GET['siruta']) || $_GET['siruta'] <
     $siruta = 40;
 else
     $siruta = $_GET['siruta'];
-    
 
 $MyObject = new SimpleSQL( $dbs, $dbu, $dbp, $db, 0, 0 );
-$MyObject->Query("SELECT * FROM `siruta` FULL JOIN `localitate` ON `localitate`.`siruta` = `_siruta` WHERE `_siruta` =".$siruta." LIMIT 1");
-$village_data = $MyObject->getCurrentLine();
-if ($village_data == -1) {
-    echo "We don't have any data for this place yet.";
-    exit(1);
+
+function village_data($siruta)
+{
+    global $MyObject;
+    
+    $MyObject->Query("SELECT * FROM `siruta` FULL JOIN `localitate` ON `localitate`.`siruta` = `_siruta` WHERE `_siruta` =".$siruta." LIMIT 1");
+    $village_data = $MyObject->getCurrentLine();
+    if ($village_data == -1) {
+        echo "We don't have any data for this place yet.";
+        exit(1);
+    }
+    
+    return $village_data;
 }
 
-$MyObject->Query("SELECT `denloc`,`jud` FROM `siruta` WHERE `siruta`.`_siruta`=".$village_data["sirsup"]." LIMIT 1");
-$county = $MyObject->getElement(0, 'denloc');
-$countyid = $MyObject->getElement(0, 'jud');
-$county = capitalize_counties($county);
-$county = str_ireplace("Județul ", "", $county);
-if ($county == -1) {
-    echo "Problem fetching county";
-    exit(1);
-}
-
-$MyObject->Query("SELECT `_siruta`, `denloc`, `codp` FROM `siruta` WHERE `sirsup`=".$village_data['siruta']);
-$uat_data = $MyObject->getTable();
-if ($uat_data == -1) {
-    echo "Problem fetching UAT data";
-    exit(1);
-}
-
-$MyObject->Query("SELECT `_siruta`, `denloc` FROM `siruta` WHERE `sirsup`=".$village_data['sirsup']);
-$commune_list = $MyObject->getTable();
-if ($commune_list == -1) {
-    echo "Problem fetching UAT data";
-    exit(1);
-}
-
-$MyObject->Query("SELECT * FROM `imagini` WHERE `siruta`=".$siruta." ORDER BY RAND() DESC LIMIT 12");
-$images = $MyObject->getTable();
-if ($images == -1) {
-    echo "Problem fetching image data";
-    exit(1);
-}
-
-$MyObject->Query("SELECT * FROM `demografie` WHERE `siruta`=".$village_data['siruta']." ORDER BY `an` DESC");
-$pop = $MyObject->getTable();
-if ($pop == -1) {
-    echo "Problem fetching population data";
-    exit(1);
-}
-
-$MyObject->Query("SELECT * FROM `oameni` WHERE `siruta`=".$siruta." ORDER BY `an` DESC");
-$leaders = $MyObject->getTable();
-if ($leaders == -1) {
-    echo "Problem fetching county president";
-    exit(1);
-}
-
-$mayoryear = 0;
-$viceyear = 0;
-$vice = Array();
-foreach ($leaders as $leader) {
-    switch($leader["functie"]) {
-        case 1:
-            if ($mayoryear)
-                continue;//TODO: history
-            $mayor = $leader['nume'];
-            $mayorparty = $leader['partid'];
-            $mayoryear = $leader['an'];
-            $mayorid = $leader['agenda_id'];
-        break;
-        case 2:
-            if ($viceyear != 0 && $viceyear != $leader['an'])
-                continue;//TODO: history
-            array_push($vice, Array( "name" => $leader['nume'],  "party" => $leader['partid']));
-            $viceyear = $leader['an'];
-            $viceid = $leader['agenda_id'];
-        break;
+function village_county_data($village_data, &$county, &$countyid)
+{
+    global $MyObject;
+    
+    $MyObject->Query("SELECT `denloc`,`jud` FROM `siruta` WHERE `siruta`.`_siruta`=".$village_data["sirsup"]." LIMIT 1");
+    $county = $MyObject->getElement(0, 'denloc');
+    $countyid = $MyObject->getElement(0, 'jud');
+    $county = capitalize_counties($county);
+    $county = str_ireplace("Județul ", "", $county);
+    if ($county == -1) {
+        echo "Problem fetching county";
+        exit(1);
     }
 }
 
-//TODO: local council
-
-$MyObject->Query("SELECT * FROM `monumente` LEFT JOIN `imagini` ON monumente.imagine = imagini.index WHERE monumente.`siruta`=".$siruta." ORDER BY RAND() LIMIT 10");
-$monuments = $MyObject->getTable();
-if ($monuments == -1) {
-        echo "Problem fetching monument data";
+function village_uat_data($siruta)
+{
+    global $MyObject;
+    
+    $MyObject->Query("SELECT `_siruta`, `denloc`, `codp` FROM `siruta` WHERE `sirsup`=".$siruta);
+    $uat_data = $MyObject->getTable();
+    if ($uat_data == -1) {
+        echo "Problem fetching UAT data";
         exit(1);
+    }
+    
+    return $uat_data;
 }
+
+function village_other_villages($village_data)
+{
+    global $MyObject;
+    
+    $MyObject->Query("SELECT `_siruta`, `denloc` FROM `siruta` WHERE `sirsup`=".$village_data['sirsup']);
+    $commune_list = $MyObject->getTable();
+    if ($commune_list == -1) {
+        echo "Problem fetching UAT data";
+        exit(1);
+    }
+    
+    return $commune_list;
+}
+
+function village_images($siruta)
+{
+    global $MyObject;
+
+    $MyObject->Query("SELECT * FROM `imagini` WHERE `siruta`=".$siruta." ORDER BY RAND() DESC LIMIT 12");
+    $images = $MyObject->getTable();
+    if ($images == -1) {
+        echo "Problem fetching image data";
+        exit(1);
+    }
+    
+    return $images;
+}
+
+function village_population($siruta)
+{
+    global $MyObject;
+
+    $MyObject->Query("SELECT * FROM `demografie` WHERE `siruta`=".$siruta." ORDER BY `an` DESC");
+    $pop = $MyObject->getTable();
+    if ($pop == -1) {
+        echo "Problem fetching population data";
+        exit(1);
+    }
+    
+    return $pop;
+}
+
+function village_leaders($siruta)
+{
+    global $MyObject;
+
+    $MyObject->Query("SELECT * FROM `oameni` WHERE `siruta`=".$siruta." ORDER BY `an` DESC");
+    $leaders = $MyObject->getTable();
+    if ($leaders == -1) {
+        echo "Problem fetching county president";
+        exit(1);
+    }
+    
+    return $leaders;
+}
+
+
+function parse_village_leaders($leaders, 
+                                &$mayor, &$mayorparty, &$mayoryear, &$mayorid,
+                                &$vice, &$viceyear, &$viceid)
+{
+    $mayoryear = 0;
+    $viceyear = 0;
+    $vice = Array();
+    foreach ($leaders as $leader) {
+        switch($leader["functie"]) {
+            case 1:
+                if ($mayoryear)
+                    continue;//TODO: history
+                $mayor = $leader['nume'];
+                $mayorparty = $leader['partid'];
+                $mayoryear = $leader['an'];
+                $mayorid = $leader['agenda_id'];
+            break;
+            case 2:
+                if ($viceyear != 0 && $viceyear != $leader['an'])
+                    continue;//TODO: history
+                array_push($vice, Array( "name" => $leader['nume'],  "party" => $leader['partid']));
+                $viceyear = $leader['an'];
+                $viceid = $leader['agenda_id'];
+            break;
+        }
+    }
+    //TODO: local council
+}
+
+function village_monuments($siruta)
+{
+    global $MyObject;
+
+    $MyObject->Query("SELECT * FROM `monumente` LEFT JOIN `imagini` ON monumente.imagine = imagini.index WHERE monumente.`siruta`=".$siruta." ORDER BY RAND() LIMIT 10");
+    
+    $monuments = $MyObject->getTable();
+    if ($monuments == -1) {
+            echo "Problem fetching monument data";
+            exit(1);
+    }
+    
+    return $monuments;
+}
+
+
+$village_data = village_data($siruta);
+village_county_data($village_data, $county, $countyid);
+$uat_data = village_uat_data($siruta);
+$commune_list = village_other_villages($village_data);
+$images = village_images($siruta);
+$pop = village_population($siruta);
+$leaders = village_leaders($siruta);
+$monuments = village_monuments($siruta);
+
+parse_village_leaders($leaders, 
+                    $mayor, $mayorparty, $mayoryear, $mayorid,
+                    $vice, $viceyear, $viceid);
 
 $smarty->assign('name', mb_convert_case($village_data['denloc'], MB_CASE_TITLE));
 $smarty->assign('county', $county);
