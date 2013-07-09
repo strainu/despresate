6bba01e032f82647a8a7757766a18d952773a91f
Andrei Cipu <github@strainu.ro>
2013-07-09 23:54:39 +0300

    Add final 2011 census data.

--- scripts/population/hist_pop.py
+++ scripts/population/hist_pop.py
@@ -57,30 +57,41 @@ def find_siruta():
     _csv = sirutalib.SirutaDatabase()
     #TODO: this is based on the assumption that we don't have 2 communes with the same name in the same county
     for elem in _csv._data:
-	index = _csv._data[elem]["name"].translate(dia_trans).replace("MUNICIPIUL ", "").replace("ORAS ", "").replace("ORASUL ", "").replace("-", " ") + \
-		_csv.get_county_string(elem).translate(dia_trans).replace("-", " ")
-        if _csv._data[elem]['type'] <= 5:  # communes
+	index = _csv._data[elem]["name"].translate(dia_trans).replace("MUNICIPIUL ", "").replace("ORAS ", "").replace("ORASUL ", "").replace("-", " ").lstrip() + \
+		_csv.get_county_string(elem).translate(dia_trans)
+        if _csv._data[elem]['type'] <= 6:  # communes
 	    if not index in data:
 	        data[index] = elem
         if index == "BUCURESTIMUNICIPIUL BUCURESTI": #Bucuresti is always special...
             data[index] = elem
         if _csv._data[elem]['type'] == 40: # counties
+	    #print _csv.get_county_string(elem).translate(dia_trans).replace("-", " ")
 	    data[_csv.get_county_string(elem).translate(dia_trans).replace("-", " ")] = elem
 	#print index
-    cr = csv.reader(open("../../data/commune_historic_population_base.csv", "rb"))
-    cw = csv.writer(open("../../data/commune_historic_population.csv", "wb"))
+    #old data
+    #cr = csv.reader(open("../../data/commune_historic_population_base.csv", "rb"))
+    #cw = csv.writer(open("../../data/commune_historic_population.csv", "wb"))
+    #2011 data
+    cr = csv.reader(open("../../data/pop2011.csv", "rb"))
+    cw = csv.writer(open("../../data/pop2011_final.csv", "wb"))
     #print data
     for line in cr:
+	if line[0] == "ROMANIA":
+            continue
 	if line[0][:7] == "JUDETUL" or line[0] == "MUNICIPIUL BUCURESTI":
 	    county = line[0]
             index = line[0].replace("-", " ")
+	    #print index
         else:
-	    index = line[0].replace("MUNICIPIUL ", "").replace("ORAS ", "").replace("ORASUL ", "").replace("-", " ") + county
+	    index = line[0].replace("MUNICIPIUL ", "").replace("ORAS ", "").replace("ORASUL ", "").replace("-", " ").lstrip() + county
 	if index in data:
 	    #print index + str(data[index])
             line.append(data[index])
             if line[2] <> "":
                 print "INSERT INTO `demografie`(`siruta`, `an`, `populatie`) VALUES(" + str(data[index]) +", " + str(line[1]) + ", " + str(line[2]) + ") ON DUPLICATE KEY UPDATE an=an;" 
+	else:
+            print "# " + county + str(line) + index
+	    line.append(0)
 	cw.writerow(line)
     pass
 
